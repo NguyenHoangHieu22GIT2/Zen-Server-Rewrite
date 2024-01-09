448be4cf8218cd5212ce0c65ea5c7ca44c6bb33d
NguyenHoangHieu22GIT2 <hoanghieufro@gmail.com>
2024-01-09 11:08:09 +0700

    backup

---
+++ src/cores/redisKeys/user.redis.keys.ts
@@ -0,0 +1,7 @@
+//HASH
+export const userKey = (email: string) => `user#${email}`;
+
+// HYPERLOGLOG
+export const usersRecentlyLoginKey = () => `usersRecentlyLogin`;
+export const usersHaveRegisteredKey = () => `usersHaveRegistered`;
+export const userFriendsKey = (email: string) => `userFriends#${email}`;
