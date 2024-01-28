a88ae3bf8e97e95d6b391e83fc49741b84a27f8d
NguyenHoangHieu <hoanghieufro@gmail.com>
2024-01-28 18:31:28 +0700

    Fix Post Module Dependencies

--- src/modules/feeds/post/post.module.ts
+++ src/modules/feeds/post/post.module.ts
@@ -4,10 +4,11 @@ import { PostServiceStable } from './stable/post.stable.service';
 import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './entities/post.entity';
+import { PostRedisStableService } from './stable/post.redis.stable.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostServiceUnstable, PostServiceStable],
+  providers: [PostServiceUnstable, PostServiceStable, PostRedisStableService],
   imports: [
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
   ],
