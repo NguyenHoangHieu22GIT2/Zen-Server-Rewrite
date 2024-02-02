c6f8d8921548b8526c5be2e137a6c1d6c342cef0
NguyenHoangHieu <hoanghieufro@gmail.com>
2024-02-02 19:29:22 +0700

    Fix the index.ts in enduser.controller.ts

--- src/modules/users/enduser/services/enduser.unstable.service.ts
+++ src/modules/users/enduser/services/enduser.unstable.service.ts
@@ -3,10 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { EndUser } from '../entities/enduser.entity';
 import { Model } from 'mongoose';
 import { EndUserId } from 'src/common/types/utilTypes/Brand';
-import { checkImageTypeToThrowErrors } from 'src/common/utils/checkImageType';
+import { checkImageTypeToThrowError, removeFile } from 'src/common/utils/index';
 import { createImageName } from 'src/common/utils/createImageName';
 import { storeFile } from 'src/common/utils/storeFile';
-import { deleteFile } from 'src/common/utils/removeFile';
 import { EnduserServiceStable } from './enduser.stable.service';
 import { ChangeInformationDto } from '../dto/change-information.dto';
 @Injectable()
@@ -28,11 +27,11 @@ export class EnduserServiceUnstable {
     file: Express.Multer.File;
     userId: EndUserId;
   }) {
-    checkImageTypeToThrowErrors(file);
+    checkImageTypeToThrowError(file);
     const fileName = createImageName(file.originalname);
     const user = await this.enduserServiceStable.findById(userId);
     storeFile({ fileName, file });
-    deleteFile(user.avatar);
+    removeFile(user.avatar);
     user.avatar = fileName;
     return user.save();
   }
