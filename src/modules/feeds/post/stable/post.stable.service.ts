0fbf0e5cae2e0417d8686b251481ba9b1ad2cdf4
NguyenHoangHieu <hoanghieufro@gmail.com>
2024-02-02 18:27:54 +0700

    move the createImageOjectsToSave to a new file in common/utils folder

--- src/modules/feeds/post/stable/post.stable.service.ts
+++ src/modules/feeds/post/stable/post.stable.service.ts
@@ -1,17 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create-post.dto';
-import { UpdatePostDto } from '../dto/update-post.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, PipelineStage } from 'mongoose';
 import { Post } from '../entities/post.entity';
 import { FindPostDto } from '../dto/find-post.dto';
 import { nameOfCollections } from 'src/common/constants/name-of-collections';
 import { PostAggregation } from 'src/common/types/mongodbTypes/aggregationTypes/feeds/post.aggregation';
-import { getPostsDto } from '../dto/get-posts.dto';
 import { EndUserId, PostId } from 'src/common/types/utilTypes/Brand';
 import { ModifyPostDto } from '../dto/modify-post.dto';
 import { DocumentMongodbType } from 'src/common/types/mongodbTypes/DocumentMongodbType';
-import { createImageName } from 'src/common/utils/createImageName';
 import { QueryLimitSkip } from 'src/cores/global-dtos/query-limit-skip.dto';
 
 @Injectable()
@@ -20,7 +17,7 @@ export class PostServiceStable {
     @InjectModel(Post.name) private readonly postModel: Model<Post>,
   ) {}
 
-  private queryAggregation: PipelineStage[] = [
+  private lookupUserAggregation: PipelineStage[] = [
     {
       $lookup: {
         from: nameOfCollections.EndUser,
@@ -46,17 +43,18 @@ export class PostServiceStable {
   public async getPostsAggregation({
     queryLimitSkip,
     //TODO: endUserId will be used to filter out which post that user should see
-    endUserId,
+    queryAggregation,
   }: {
     queryLimitSkip: QueryLimitSkip;
-    endUserId: EndUserId;
+    queryAggregation: PipelineStage[];
   }) {
     const postsAggregation: PostAggregation[] = await this.postModel.aggregate([
       {
         $limit: queryLimitSkip.limit,
         $skip: queryLimitSkip.skip,
       },
-      ...this.queryAggregation,
+      ...queryAggregation,
+      ...this.lookupUserAggregation,
     ]);
     return postsAggregation;
   }
@@ -65,7 +63,7 @@ export class PostServiceStable {
       {
         $match: { _id: findPostDto.postId },
       },
-      ...this.queryAggregation,
+      ...this.lookupUserAggregation,
     ]);
     const postAggregation = postsAggregation[0];
     return postAggregation;
@@ -95,17 +93,6 @@ export class PostServiceStable {
     return createdPost;
   }
 
-  public createImageObjectsToSave(images: Express.Multer.File[]) {
-    const imageNames = [];
-    const createdImageObjects = images.map((image) => {
-      const imageName = createImageName(image.originalname);
-      imageNames.push(imageName);
-      // file and fileName property because storeFile function demands it
-      return { file: image, fileName: imageName };
-    });
-    return { createdImageObjects, imageNames };
-  }
-
   public async modifyPost({
     endUserId,
     modifyPostDto,
@@ -121,11 +108,10 @@ export class PostServiceStable {
     });
 
     // Do this so we don't need to keep changing if we add some new properties
-    const { postId, ...modifiedData } = modifyPostDto;
 
     const modifiedPost = await this.postModel.findByIdAndUpdate(
       modifyPostDto.postId,
-      modifiedData,
+      modifyPostDto,
     );
     return modifiedPost;
   }
