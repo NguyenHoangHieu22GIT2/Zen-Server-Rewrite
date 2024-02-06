a0e90a0d8a7682eeb02dc1a3a4cf08ee6586b54c
NguyenHoangHieu22GIT2 <hoanghieufro@gmail.com>
2024-02-06 20:40:26 +0700

    Finish Post spec

--- src/modules/feeds/post/post.controller.spec.ts
+++ src/modules/feeds/post/post.controller.spec.ts
@@ -9,6 +9,7 @@ import { faker } from '@faker-js/faker';
 import { EndUser } from 'src/modules/users/enduser/entities/enduser.entity';
 import { RequestUser } from 'src/common/types/utilTypes/RequestUser';
 import { createFakeImage } from 'src/common/utils/createFakeImage';
+import { PostRedisStableService } from './services/stable/post.redis.stable.service';
 
 describe('PostController', () => {
   let controller: PostController;
@@ -16,13 +17,14 @@ describe('PostController', () => {
   // let id: string;
   let userToTest: Partial<EndUser>;
   let fakeRequestUser: Partial<RequestUser>;
+  let postToTest: Post;
 
   beforeAll(async () => {
     mongod = await connect('mongodb://127.0.0.1:27017/', {
       dbName: 'Zen-Test',
     });
     userToTest = (await mongod.connection.db
-      .collection('users')
+      .collection('endusers')
       .findOne()) as any;
     // id = v4();
     fakeRequestUser = {
@@ -34,7 +36,11 @@ describe('PostController', () => {
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
-      providers: [PostServiceUnstable, PostServiceStable],
+      providers: [
+        PostServiceUnstable,
+        PostServiceStable,
+        PostRedisStableService,
+      ],
       imports: [
         MongooseModule.forRoot('mongodb://127.0.0.1:27017/', {
           dbName: 'Zen-Test',
@@ -64,6 +70,7 @@ describe('PostController', () => {
       fakeRequestUser as any,
       [image],
     );
+    postToTest = post;
     expect(post).toBeDefined();
   });
 
@@ -74,5 +81,43 @@ describe('PostController', () => {
       skip: 0,
     });
     expect(posts).toBeDefined();
+  });
+
+  it('should get recommend posts', async () => {
+    const posts = await controller.getRecommendedPosts(fakeRequestUser as any, {
+      limit: 10,
+      skip: 0,
+    });
+    expect(posts).toBeDefined();
+  });
+
+  it('should get one post', async () => {
+    const post = await controller.getPost(fakeRequestUser as any, {
+      postId: postToTest._id,
+    });
+    expect(post).toBeDefined();
+  });
+
+  it('should be able to modify a post', async () => {
+    const title = faker.color.human();
+    const body = faker.lorem.paragraph(5);
+    const image = createFakeImage();
+    const post = await controller.modifyPost(
+      fakeRequestUser as any,
+      {
+        postId: postToTest._id,
+        title,
+        body,
+      },
+      [image],
+    );
+    expect(post.title).toEqual(title);
+  });
+
+  it('should delete a post', async () => {
+    const post = await controller.deletePost(fakeRequestUser as any, {
+      postId: postToTest._id,
+    });
+    expect(post).toBeDefined();
   });
 });
