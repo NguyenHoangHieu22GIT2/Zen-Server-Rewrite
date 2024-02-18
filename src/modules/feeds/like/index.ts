d40bc2347bf73b41bb2f0833537f54cf7c5ba0dd
hoanghieu <hoanghieufro@gmail.com>
2024-02-18 21:15:21 +0700

    Create index.ts file for every folder that needs it

---
+++ src/modules/feeds/like/index.ts
@@ -0,0 +1,5 @@
+export * from './like.controller';
+export * from './like.module';
+export * from './entities';
+export * from './services/stable';
+export * from './services/unstable';
---
+++ src/modules/users/enduser/index.ts
@@ -0,0 +1,6 @@
+export * from './enduser.controller';
+export * from './enduser.module';
+export * from './dto';
+export * from './entities';
+export * from './services/stable';
+export * from './services/unstable';
